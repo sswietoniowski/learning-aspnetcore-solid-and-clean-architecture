0a2fd7161d4a0871dd2781303e1a2ffc3cbb0a34
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-01-31 19:23:13 +0100

    Updated mapper configuration

--- HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -4,16 +4,24 @@ using HR.LeaveManagement.Application.DTOs.LeaveRequest;
 using HR.LeaveManagement.Application.DTOs.LeaveType;
 using HR.LeaveManagement.Domain;
 
-namespace HR.LeaveManagement.Application
+namespace HR.LeaveManagement.Application.Profiles
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
+            CreateMap<LeaveRequest, ILeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, ILeaveAllocationDto>().ReverseMap();
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, ILeaveTypeDto>().ReverseMap();
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
         }
     }
 }
