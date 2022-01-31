9594a2f04d74edadc3d18b2360478929f9d53869
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-01-31 19:24:36 +0100

    Using regions for better organization of mappings

--- HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -10,15 +10,19 @@ namespace HR.LeaveManagement.Application.Profiles
     {
         public MappingProfile()
         {
+            #region LeaveRequest Mappings
             CreateMap<LeaveRequest, ILeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
             CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+            #endregion
+
             CreateMap<LeaveAllocation, ILeaveAllocationDto>().ReverseMap();
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
             CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
             CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+
             CreateMap<LeaveType, ILeaveTypeDto>().ReverseMap();
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
             CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
