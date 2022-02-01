5360fd2117bf5f1ae41bd4b4eb87f67d49169de3
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-01 12:02:05 +0100

    Added EFCore dependencies, created db context, defined custom saving for auditing purposes

--- HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
+++ HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -5,7 +5,7 @@ namespace HR.LeaveManagement.Domain.Common
     public abstract class BaseDomainEntity
     {
         public int Id { get; set; }
-        public DateTime DateCreated { get; set; }
+        public DateTime CreatedDate { get; set; }
         public string CreatedBy { get; set; }
         public DateTime LastModifiedDate { get; set; }
         public string LastModifiedBy { get; set; }
---
+++ HR.LeaveManagement.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public DbSet<LeaveAllocation> LeaveAllocation { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.CreatedDate = DateTime.Now;
+                }
+            }
+            
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
