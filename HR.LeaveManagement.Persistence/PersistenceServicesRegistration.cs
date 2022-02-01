76adb4ad8395b2a8577568574dcd6b21a50409f1
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-01 12:36:07 +0100

    Added persistence services configuration

---
+++ HR.LeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,26 @@
+﻿using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString"));
+            });
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+            return services;
+        }
+    }
+}
