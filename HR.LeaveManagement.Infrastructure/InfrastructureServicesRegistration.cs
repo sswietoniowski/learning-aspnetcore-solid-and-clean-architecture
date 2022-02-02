305d25064ff5ba552a598be8ef941a4297a9bec4
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-02 18:25:36 +0100

    Added infrastructure services registration

---
+++ HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using HR.LeaveManagement.Application.Contracts.Infrastructure;
+using HR.LeaveManagement.Application.Models;
+using HR.LeaveManagement.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            return services;
+        }
+    }
+}
