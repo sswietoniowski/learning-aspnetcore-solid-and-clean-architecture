43bb30e3d720e99df932d91acc22ca2a4746a10a
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-05 19:52:01 +0100

    Added 1st unit test

---
+++ HR.LeaveManagement.Application.UnitTests/LeaveTypes/Queries/GetLeaveTypeListRequestHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries;
+using HR.LeaveManagement.Application.Profiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Xunit;
+
+namespace HR.LeaveManagement.Application.UnitTests.LeaveTypes.Queries
+{
+    public class GetLeaveTypeListRequestHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ILeaveTypeRepository> _mockRepository;
+
+        public GetLeaveTypeListRequestHandlerTests()
+        {
+            _mockRepository = MockLeaveTypeRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypeListRequestHandler(_mockRepository.Object, _mapper);
+        }
+    }
+}
