36e07944e80ea15b5503e0b8e0c7658617ce5c40
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-05 19:43:27 +0100

    Added test project and first mock repository

---
+++ HR.LeaveManagement.Application.UnitTests/Mocks/MockRepositories.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks
+{
+    public static class MockLeaveTypeRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>
+            {
+                new LeaveType
+                {
+                    Id = 1,
+                    Name = "Test Vacation",
+                    DefaultDays = 10
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    Name = "Test Sick",
+                    DefaultDays = 15
+                }
+            };
+
+            var mockRepository = new Mock<ILeaveTypeRepository>();
+            mockRepository.Setup(r => r.GetAll()).ReturnsAsync(leaveTypes);
+            mockRepository.Setup(r => r.Add(It.IsAny<LeaveType>())).ReturnsAsync((LeaveType leaveType) =>
+            {
+                leaveTypes.Add(leaveType);
+                return leaveType;
+            });
+
+            return mockRepository;
+        }
+    }
+}
