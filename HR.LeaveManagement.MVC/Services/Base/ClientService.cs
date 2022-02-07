c4565da0000cf5271298d8bfee1263c893b162f8
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-07 21:28:59 +0100

    Fixed Client class genereated by NSwag, should regenerate it  instead

--- HR.LeaveManagement.MVC/Services/Base/ClientService.cs
+++ HR.LeaveManagement.MVC/Services/Base/ClientService.cs
@@ -173,9 +173,8 @@ namespace HR.LeaveManagement.MVC.Services.Base
         private System.Net.Http.HttpClient _httpClient;
         private System.Lazy<Newtonsoft.Json.JsonSerializerSettings> _settings;
 
-        public Client(string baseUrl, System.Net.Http.HttpClient httpClient)
+        public Client(System.Net.Http.HttpClient httpClient)
         {
-            BaseUrl = baseUrl;
             _httpClient = httpClient;
             _settings = new System.Lazy<Newtonsoft.Json.JsonSerializerSettings>(CreateSerializerSettings);
         }
