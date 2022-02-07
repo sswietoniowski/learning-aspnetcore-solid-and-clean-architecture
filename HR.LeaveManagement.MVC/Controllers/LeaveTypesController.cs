e405e3f492a1e0cc15ad67c16fc57e61b539a9bd
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-07 21:10:26 +0100

    Added leave types controller

---
+++ HR.LeaveManagement.MVC/Controllers/LeaveTypesController.cs
@@ -0,0 +1,91 @@
+﻿using HR.LeaveManagement.MVC.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR.LeaveManagement.MVC.Controllers
+{
+    public class LeaveTypesController : Controller
+    {
+        private readonly ILeaveTypeService _leaveTypeService;
+
+        public LeaveTypesController(ILeaveTypeService leaveTypeService)
+        {
+            this._leaveTypeService = leaveTypeService;
+        }
+
+        // GET: LeaveTypesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LeaveTypesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LeaveTypesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveTypesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveTypesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
