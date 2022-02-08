746d6ad216becb484fe3165877afab9e19deb8d6
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-08 20:48:58 +0100

    Added authentication service

---
+++ HR.LeaveManagement.MVC/Contracts/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.MVC.Contracts
+{
+    public interface IAuthenticationService
+    {
+        Task<bool> Authenticate(string email, string password);
+        Task<bool> Register(string firstName, string lastName, string userName, string email, string password);
+        Task Logout();
+    }
+}
---
+++ HR.LeaveManagement.MVC/Services/AuthenticateService.cs
@@ -0,0 +1,108 @@
+﻿using HR.LeaveManagement.MVC.Contracts;
+using HR.LeaveManagement.MVC.Services.Base;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.MVC.Services
+{
+    public class AuthenticateService : BaseHttpService, Contracts.IAuthenticationService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private JwtSecurityTokenHandler _tokenHandler;
+
+        public AuthenticateService(IClient client, ILocalStorageService localStorage,
+            IHttpContextAccessor httpContextAccessor) : base(client, localStorage)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _tokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            try
+            {
+                AuthRequest authRequest = new()
+                {
+                    Email = email,
+                    Password = password
+                };
+
+                var authResponse = await _client.LoginAsync(authRequest);
+
+                if (authResponse?.Token != String.Empty)
+                {
+                    var tokenContent = _tokenHandler.ReadJwtToken(authResponse.Token);
+                    var claims = ParseClaims(tokenContent);
+                    var user = new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme));
+                    var login = _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, user);
+                    _localStorage.SetStorageValue("token", authResponse.Token);
+
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task Logout()
+        {
+            try
+            {
+                _localStorage.ClearStorage(new List<string> { "token" });
+                await _httpContextAccessor.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Register(string firstName, string lastName, string userName, string email, string password)
+        {
+            try
+            {
+                RegistrationRequest registrationRequest = new()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    UserName = userName,
+                    Email = email,
+                    Password = password
+                };
+
+                var response = await _client.RegisterAsync(registrationRequest);
+
+                if (!string.IsNullOrEmpty(response?.UserId))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private IList<Claim> ParseClaims(JwtSecurityToken tokenContent)
+        {
+            var claims = tokenContent.Claims.ToList();
+            claims.Add(new Claim(ClaimTypes.Name, tokenContent.Subject));
+            return claims;
+        }
+    }
+}
