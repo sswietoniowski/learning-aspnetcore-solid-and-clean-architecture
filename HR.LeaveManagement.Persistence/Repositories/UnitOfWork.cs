ba3a2c1f2c8b908efae40f3ba48f26320e1e054e
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-11 10:38:36 +0100

    Added UoW support for batch operations

---
+++ HR.LeaveManagement.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LeaveManagementDbContext _context;
+
+        private ILeaveAllocationRepository _leaveAllocationRepository;
+        private ILeaveRequestRepository _leaveRequestRepository;
+        private ILeaveTypeRepository _leaveTypeRepository;
+
+        public ILeaveAllocationRepository LeaveAllocationRepository => _leaveAllocationRepository ??= new LeaveAllocationRepository(_context);
+
+        public ILeaveRequestRepository LeaveRequestRepository => _leaveRequestRepository ??= new LeaveRequestRepository(_context);
+
+        public ILeaveTypeRepository LeaveTypeRepository => _leaveTypeRepository ??= new LeaveTypeRepository(_context);
+
+        public UnitOfWork(LeaveManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
